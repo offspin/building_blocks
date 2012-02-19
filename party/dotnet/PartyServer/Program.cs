8d4992a5b802ad6bc654229b2cd9f5354a6286b5
Neil Sykes <nsykes@development.offspin.com>
2012-02-19 16:42:47 +0000

    Add new message processor behaviour to WCF service

--- party/dotnet/PartyServer/Program.cs
+++ party/dotnet/PartyServer/Program.cs
@@ -19,6 +19,8 @@ namespace PartyServer
                 typeof(PartyService.IService), 
                 serviceBinding, 
                 "http://localhost:8000/service");
+            ep.Behaviors.Add(new PartyService.ProcessorBehaviour());
+           
   
             WebHttpBinding staticBinding = new WebHttpBinding(WebHttpSecurityMode.None);
             ServiceEndpoint sep = webServiceHost.AddServiceEndpoint(
